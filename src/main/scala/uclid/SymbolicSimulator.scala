c12119e75501bfbc966c44756c9d74193cfd681c
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2023-10-24 13:27:02 -0700

    Assert existence of ensures and requires

--- src/main/scala/uclid/SymbolicSimulator.scala
+++ src/main/scala/uclid/SymbolicSimulator.scala
@@ -1565,6 +1565,11 @@ class SymbolicSimulator (module : Module) {
       else
       println("Would have used CBMC for this procedure, but it is not supported yet.")
     }    
+    if (proc.annotations.ids.contains(Identifier("svcomp-verifier"))) {
+      // TODO: create special logic for inlined statements. 
+      // At the moment, we opt to enforce the existence of pre-/post-conditions to reduce inline to no-inline case.
+      assert(proc.requires.length > 0 && proc.ensures.length > 0)
+    }
     assertionTree.startVerificationScope()
 
     val procScope = context + proc
